d8e12cd0d1e4c3c1b34b38e50512db93f5bbe418
diyordaniyor <diyordaniyor@mail.ru>
2022-12-06 20:17:27 -0500

    Updated format Batch20.java

--- src/Batch20.java
+++ src/Batch20.java
@@ -1,5 +1,8 @@
 public class Batch20 {
     public static void main(String[] args) {
         System.out.println("I am so happy");
+        System.out.println("Today is a good day");
+        int v = 25;
+        System.out.println(v);
     }
 }
